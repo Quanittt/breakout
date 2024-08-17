ce8ad21f04070976e916930eebe6387f5cdf489b
menta_de_gly <maksimlozinskiy12@gmail.com>
2024-08-18 00:40:57 +0300

    create a paddle & make moving the paddle

--- index.js
+++ index.js
@@ -2,20 +2,56 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const btn_start = document.getElementById("btn_start");
 
+let right = false;
+let left = false;
+
 // Ball's position
 let x = canvas.width / 2;
 let y = canvas.height - 40;
 let dx = 2;
 let dy = -2;
 
+// Paddle position
+const paddleHeight = 10;
+const paddleWidth = 75;
+let paddleX = (canvas.width - paddleWidth) / 2;
+let paddleY = canvas.height - paddleHeight;
+
+
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, paddleY, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+document.addEventListener("keydown", keyIsPressed, false);
+
+function keyIsPressed(btn) {
+    if (btn.key === "Right" || btn.key === "ArrowRight") {
+        paddleX += 7;
+        paddleX = Math.min(paddleX + 7, canvas.width - paddleWidth);
+    } else if (btn.key === "Left" || btn.key === "ArrowLeft") {
+        paddleX -= 7;
+        paddleX = Math.max(paddleX - 7, 0); 
+    }
+}
+
 // Function for ball's moving
-function draw() {
+function drawBall() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    // drawing a ball
     ctx.beginPath();
     ctx.arc(x, y, 10, 0, Math.PI * 2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
+
+    drawPaddle()
+    
+    //change ball's pos
     y += dy;
     x += dx;
     
@@ -24,23 +60,11 @@ function draw() {
         dx = -dx;
     }
     
-    if (y + dy > canvas.height - 10 || y + dy < 10) {
+    if (y + dy < 10) {
         dy = -dy;
     }      
 }
-setInterval(draw, 10);
+
 btn_start.addEventListener("click", () => {
+    setInterval(drawBall, 10);
 });
-
-ctx.beginPath();
-ctx.rect(20, 20, 100, 20);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
-
-
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-ctx.stroke();
-ctx.closePath();
